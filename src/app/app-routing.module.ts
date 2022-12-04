c18945e2bd0596ed89028a49d62cee1193fea378
Julian <julian.barnes100@gmail.com>
2022-12-03 19:59:32 -0500

    Initial commit for local development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { EventAddComponent } from './views/event-add/event-add.component';
+import { EventDetailComponent } from './views/event-detail/event-detail.component';
+import { EventsBrowseComponent } from './views/events-browse/events-browse.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/events', pathMatch: 'full'},
+  { path: 'events', component: EventsBrowseComponent},
+  { path: 'event-detail/:eventId', component: EventDetailComponent},
+  { path: 'events/add', component: EventAddComponent},
+  { path: 'test', component: TestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
